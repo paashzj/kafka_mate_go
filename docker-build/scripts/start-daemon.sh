8ddc35c2a785d5e899d4f30102f969164b757c73
lovehzj <1922919664@qq.com>
2022-01-14 20:06:15 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-mkdir $KAFKA_HOME/logs
+mkdir -p $KAFKA_HOME/logs
 nohup $KAFKA_HOME/mate/kafka_mate >>$KAFKA_HOME/logs/kafka_mate.stdout.log 2>>$KAFKA_HOME/logs/kafka_mate.stderr.log &
+
